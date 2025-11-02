887998561c337798b5174bb1a8059ac22c918409
joeykblack <joeykblack@gmail.com>
2025-11-02 15:29:16 -0500

    build one file

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 // vite.config.js
-export default {
+import { defineConfig } from 'vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
+
+export default defineConfig({
+  plugins: [viteSingleFile()],
   esbuild: {
     jsxFactory: 'h',
     jsxFragment: 'Fragment',
@@ -10,5 +14,13 @@ export default {
       'react': 'preact/compat',
       'react-dom': 'preact/compat'
     }
+  },
+  build: {
+    // Turn off code-splitting so the plugin can inline everything into one file
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
-}
+})
